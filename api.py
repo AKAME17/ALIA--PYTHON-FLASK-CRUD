0dffb170758c6033022563a7d091590e89f8b8b0
AKAME17 <nelyzagalia21@gmail.com>
2024-05-24 07:11:42 +0800

    fetching sql data and Get Student fucntion

--- api.py
+++ api.py
@@ -1,16 +1,45 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>" 
+
+# if __name__== "__main__":
+#     app.run(debug= True)  
+
+
+from flask import Flask, make_response, jsonify, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "sitedata"
+
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>" 
-
-if __name__== "__main__":
-    app.run(debug= True)  
+    return "<p>Hello, World!</p>"
 
 
-# from flask import Flask, make_response, jsonify, request
-# from flask_mysqldb import MySQL
-# from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
 
+
+@app.route("/itstudent", methods=["GET"])
+def get_actors():
+    data = data_fetch("""select * from itstudent""")
+    return make_response(jsonify(data), 200)
+
+if __name__ == "__main__":
+    app.run(debug=True) 
