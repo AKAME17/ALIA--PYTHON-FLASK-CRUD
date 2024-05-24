e7276047eaecba3abfb6a196e53364c7a1d45871
AKAME17 <nelyzagalia21@gmail.com>
2024-05-24 08:31:16 +0800

    Fourth commit of adding new student fucntion

--- api.py
+++ api.py
@@ -58,6 +58,24 @@ ON course_enrollees.courseid = course_name.courseid
 where itstudent.studentnumber = {}""".format(id))
     return make_response(jsonify({"Student number": id, "count": len(data), "courses": data}), 200)
 
+@app.route("/itstudent", methods=["POST"])
+def add_itstudent():
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    fullname = info ["fullname"]
+    studentnumber = info ["studentnumber"]
+    address = info ["address"]
+    yearandblock = info ["yearandblock"]
+    units = info ["units"]
+    cur.execute(
+        """ INSERT INTO itstudent (fullname, studentnumber, address, yearandblock, units) VALUE (%s, %s, %s, %s, %s)""", 
+        (fullname, studentnumber, address, yearandblock, units),
+    )
+    mysql.connection.commit()
+    print("Row(s) affected: {}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(jsonify({"Message": "Student added successfully", "rows_affected": rows_affected}), 201)
 
 if __name__ == "__main__":
     app.run(debug=True) 
